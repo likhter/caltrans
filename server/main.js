811d87a29375f2dc9bbb60d05da6488d4a6f49c9
Konstantin Likhter <konstantin.likhter@intel.com>
2017-02-22 20:16:36 -0800

    add package.json and server/ folder

--- server/main.js
+++ server/main.js
@@ -2,15 +2,19 @@ const express = require('express');
 const path = require('path');
 const apiRouter = require('./apiRouter');
 
-const { PORT } = require('./config');
+const { PORT, STATIC_FILES } = require('./config');
 
 const app = express();
+
+process.on('unhandledException', e => {
+  console.log('UNHANLED EXCEPTION:', e);
+});
 
 process.on('unhandledRejection', (reason, p) => {
   console.error('UNHANDLED REJECTION: Promise=', p, 'reason=', reason);
 });
 
 app.use('/api', apiRouter);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, STATIC_FILES)));
 
 app.listen(PORT);

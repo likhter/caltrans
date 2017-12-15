147a517375441b98d2314946f266af15a95112c5
Konstantin Likhter <konstantin@likhter.ru>
2017-12-14 22:58:19 -0800

    rewrite router using async/await

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -14,52 +14,50 @@ const {
   FETCH_URL
 } = require('./config');
 
-function fetchAndSave() {
-  return new Promise((resolve, reject) => {
-    fetch(FETCH_URL)
-      .then(res => res.text())
-      .then(res => parse(res.split("\n")))
-      .then(parsed => Promise.all([
-        redisClient.set(CONDITIONS_KEY, JSON.stringify(parsed.conditions)),
-        redisClient.sadd(ROADS_KEY, parsed.roads),
-        redisClient.set(UPDATE_KEY, Date.now())
-      ]))
-      .then(resolve)
-      .catch(reject);
-  });
+async function fetchAndSave() {
+    const response = await fetch(FETCH_URL);
+    const unparsed = await response.text();
+    const parsed = await parse(unparsed.split("\n"));
+    return await Promise.all([
+      redisClient.set(CONDITIONS_KEY, JSON.stringify(parsed.conditions)),
+      redisClient.sadd(ROADS_KEY, parsed.roads),
+      redisClient.set(UPDATE_KEY, Date.now())
+    ]);
 }
 
-router.get('/everything', (req, res) => {
-  Promise.all([
-    redisClient.get(CONDITIONS_KEY),
-    redisClient.smembers(ROADS_KEY),
-    redisClient.get(UPDATE_KEY)
-  ]).then(([conditions, roads, lastUpdate]) => {
-    try {
-      conditions = JSON.parse(conditions);
-    } catch(e) {
-      throw 'update is needed';
-    }
-    res.status(200).json({ 
-      conditions, 
-      roads, 
+router.get('/everything', async (req, res) => {
+  try {
+    const [conditionsString, roads, lastUpdate] = await Promise.all([
+      redisClient.get(CONDITIONS_KEY),
+      redisClient.smembers(ROADS_KEY),
+      redisClient.get(UPDATE_KEY)
+    ]);
+
+    const conditions = JSON.parse(conditionsString);
+    res.status(200).json({
+      conditions,
+      roads,
       lastUpdate,
       updateInterval: UPDATE_INTERVAL
     });
-  }).catch(e => console.log(e) && res.status(500).json({ e }));
+  } catch(err) {
+    res.status(500).json({ err });
+  }
 });
 
-router.get('/update', (req, res) => {
-  redisClient.get(UPDATE_KEY)
-    .then(update => {
-      const shouldUpdate = Date.now() - update > UPDATE_INTERVAL;
+router.get('/update', async (req, res) => {
+    try {
+      const lastUpdate = await redisClient.get(UPDATE_KEY);
+      const shouldUpdate = Date.now() - lastUpdate > UPDATE_INTERVAL;
       if (shouldUpdate) {
-        fetchAndSave().then(() => res.status(200).json({ done : true }));
+        await fetchAndSave();
+        res.status(200).json({ done: true });
       } else {
         res.status(200).json({ err: 'too often' });
       }
-    })
-    .catch(err => res.status(500).json({ err }));
+    } catch (err) {
+      res.status(500).json({ err });
+    }
 });
 
 module.exports = router;
